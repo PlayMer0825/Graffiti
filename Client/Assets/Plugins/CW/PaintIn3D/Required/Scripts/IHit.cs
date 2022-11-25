ca8d474f020b2603581366837be2300f4b410f0d
unknown <wdg2461@gmail.com>
2022-11-25 22:31:20 +0900

    Init

--- Client/Assets/Plugins/CW/PaintIn3D/Required/Scripts/IHit.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PaintIn3D
-{
-	/// <summary>All <b>IHit___</b> interfaces implement this interface so they can all be easily found with <b>GetComponent</b>.</summary>
-	public interface IHit
-	{
-	}
-}
