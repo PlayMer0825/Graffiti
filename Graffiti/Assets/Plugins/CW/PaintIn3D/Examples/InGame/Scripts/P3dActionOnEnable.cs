ab83c77206bb4bd2d7e154d25dda9edea15f38db
1n/usr/bin/bashmni4 <wdg2461@gmail.com>
2022-11-26 12:36:16 +0900

    P3D Asset Code Beautify

--- Graffiti/Assets/Plugins/CW/PaintIn3D/Examples/InGame/Scripts/P3dActionOnEnable.cs
+++ Graffiti/Assets/Plugins/CW/PaintIn3D/Examples/InGame/Scripts/P3dActionOnEnable.cs
@@ -2,42 +2,36 @@
 using UnityEngine.Events;
 using CW.Common;
 
-namespace PaintIn3D
-{
-	/// <summary>This component invokes the <b>Action</b> event when this component is enabled.</summary>
-	[HelpURL(P3dCommon.HelpUrlPrefix + "P3dActionOnEnable")]
-	[AddComponentMenu(P3dCommon.ComponentMenuPrefix + "Action OnEnable")]
-	public class P3dActionOnEnable : MonoBehaviour
-	{
-		/// <summary>The event that will be invoked.</summary>
-		public UnityEvent Action { get { if (action == null) action = new UnityEvent(); return action; } } [SerializeField] public UnityEvent action;
+namespace PaintIn3D {
+    /// <summary>This component invokes the <b>Action</b> event when this component is enabled.</summary>
+    [HelpURL(P3dCommon.HelpUrlPrefix + "P3dActionOnEnable")]
+    [AddComponentMenu(P3dCommon.ComponentMenuPrefix + "Action OnEnable")]
+    public class P3dActionOnEnable : MonoBehaviour {
+        /// <summary>The event that will be invoked.</summary>
+        public UnityEvent Action { get { if(action == null) action = new UnityEvent(); return action; } }
+        [SerializeField] public UnityEvent action;
 
-		protected virtual void OnEnable()
-		{
-			if (action != null)
-			{
-				action.Invoke();
-			}
-		}
-	}
+        protected virtual void OnEnable() {
+            if(action != null) {
+                action.Invoke();
+            }
+        }
+    }
 }
 
 #if UNITY_EDITOR
-namespace PaintIn3D
-{
-	using UnityEditor;
-	using TARGET = P3dActionOnEnable;
+namespace PaintIn3D {
+    using UnityEditor;
+    using TARGET = P3dActionOnEnable;
 
-	[CanEditMultipleObjects]
-	[CustomEditor(typeof(TARGET))]
-	public class P3dActionOnEnable_Editor : CwEditor
-	{
-		protected override void OnInspector()
-		{
-			TARGET tgt; TARGET[] tgts; GetTargets(out tgt, out tgts);
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(TARGET))]
+    public class P3dActionOnEnable_Editor : CwEditor {
+        protected override void OnInspector() {
+            TARGET tgt; TARGET[] tgts; GetTargets(out tgt, out tgts);
 
-			Draw("action");
-		}
-	}
+            Draw("action");
+        }
+    }
 }
 #endif
