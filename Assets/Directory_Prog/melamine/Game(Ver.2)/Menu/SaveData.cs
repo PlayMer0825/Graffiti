6f1798e6a569d8332dffda82d8a644f4623b8d6d
1melamine1 <117522707+1melamine1@users.noreply.github.com>
2023-03-30 16:29:03 +0900

    TiTi 추가

---
+++ Assets/Directory_Prog/melamine/Game(Ver.2)/Menu/SaveData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public int HP;
+    public float AP;
+
+    public float X;
+    public float Y;
+    public float Z;
+}
