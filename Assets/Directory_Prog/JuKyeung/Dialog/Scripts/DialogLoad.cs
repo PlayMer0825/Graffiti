dd6ddbf272dfdda12b8f837410e1344a17a888de
kashan2388 <73982847+kashan2388@users.noreply.github.com>
2023-04-03 01:16:18 +0900

    DialogLoad 코드 수정 중

--- Assets/Directory_Prog/JuKyeung/Dialog/Scripts/DialogLoad.cs
+++ Assets/Directory_Prog/JuKyeung/Dialog/Scripts/DialogLoad.cs
@@ -4,55 +4,66 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum EActorList
+{
+    Titi,
+    Red,
+    Blue,
+    Yellow,
+    NPC1
+};
+
 public class DialogLoad : MonoBehaviour
 {
-    // 저장된 대화 정보를 가져올 DialogSave 스크립트의 인스턴스
     public DialogSave dialogSave;
 
-    [SerializeField] string eventID;
+    // 이벤트 아이디 
+    [HideInInspector]
+    public string eventID_Setting;
 
-    // 이벤트 ID와 액터 ID를 인자로 전달하면 해당 대화 정보를 반환합니다.
-    public string[] GetDialog(string eventID)
+    // 현재 불러온 이벤트를 담는 배열 
+    public string[] dialogArray;
+
+    public EActorList EactorList = EActorList.Titi;
+
+    // 이벤트 ID와 액터 ID를 인자로 전달하면 해당 대화 정보를 반환.
+    private string[] GetDialog(string eventID, string actorID)
     {
         Dictionary<string, List<string>> currentDialog;
         if (dialogSave.save.TryGetValue(eventID, out currentDialog))
         {
             List<string> dialogList = new List<string>();
-            // 해당 이벤트에 속한 모든 대화 정보를 반환합니다.
-
-            foreach (KeyValuePair<string, List<string>> pair in currentDialog)
+            if (currentDialog.TryGetValue(actorID, out List<string> actorDialog))
             {
-                string actorID = pair.Key;
-                foreach (string context in pair.Value)
+                // 해당 이벤트에 속한 해당 액터의 모든 대화 정보를 반환합니다.
+                foreach (string context in actorDialog)
                 {
-                    dialogList.Add(actorID + " | " + context);
+                    dialogList.Add(context);
                 }
             }
 
             return dialogList.ToArray();
         }
 
-        // 해당 이벤트가 없으면 빈 배열을 반환합니다.
+        // 해당 이벤트가 없거나 해당 액터의 대화 정보가 없으면 빈 배열을 반환.
         return new string[0];
     }
 
     private void Start()
     {
-        // DialogSave 클래스 인스턴스를 할당합니다.
         dialogSave = FindObjectOfType<DialogSave>();
-
-        // eventID에 해당하는 대화 정보를 가져와서 개행 문자로 구분된 문자열로 출력합니다.
-        //Debug.Log(string.Join("\n", GetDialog(eventID)));
-
-        PrintDialog();
-
     }
 
-    private void PrintDialog()
+
+    /// <summary>
+    /// 이벤트 아이디를 설정할 수 있는 메서드 
+    /// </summary>
+    /// <param name="_eventID"></param>
+    /// <returns></returns>
+    public string SetEventID
     {
-        for (int i = 0; i < GetDialog(eventID).Length; i++)
-        {
-            Debug.Log(GetDialog(eventID)[i]);
-        }
+        get { return eventID_Setting; }
+        set { eventID_Setting = value; }
     }
+
 }
