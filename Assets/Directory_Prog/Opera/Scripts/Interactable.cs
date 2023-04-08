f5ae35dc7638775f65ec1e8dd051c1ad6260a038
O-pera <wdg2461@gmail.com>
2023-04-08 20:04:16 +0900

    드로잉 캔버스 초기 구성

--- Assets/Directory_Prog/Opera/Scripts/Interactable.cs
+++ Assets/Directory_Prog/Opera/Scripts/Interactable.cs
@@ -5,18 +5,12 @@ using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 namespace OperaHouse {
-
-    public struct EventFunc {
-        public bool NeedToReUse;
-        public Func<bool> EventFunction;
-    }
-
     /// <summary>
     /// 상호작용 가능한 오브젝트에 대한 부모 클래스
     /// </summary>
     public class Interactable : MonoBehaviour {
         [Header("Interactable UIs")]
-        [SerializeField] GameObject _interactCanvas = null;
+        [SerializeField] protected GameObject _interactCanvas = null;
 
         //[Header("Delegate Functions' Queue for Any Events")]
 
@@ -24,14 +18,14 @@ namespace OperaHouse {
         /// <summary>
         /// OnTriggerEnter()에서 호출하는 함수.
         /// </summary>
-        public virtual void ReadyInteract() {
+        public virtual void ReadyInteract(Collider other) {
             _interactCanvas.SetActive(true);
         }
 
         /// <summary>
         /// OnTriggerExit()에서 호출하는 함수.
         /// </summary>
-        public virtual void UnReadyInteract() {
+        public virtual void UnReadyInteract(Collider other) {
             _interactCanvas.SetActive(false);
         }
 
