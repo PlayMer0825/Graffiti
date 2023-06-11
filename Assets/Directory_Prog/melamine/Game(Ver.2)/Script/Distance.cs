2e590515e38f1d0ce9a85181e285f0fc66aa5678
1melamine1 <117522707+1melamine1@users.noreply.github.com>
2023-06-12 01:22:39 +0900

    보드 ui적용

--- Assets/Directory_Prog/melamine/Game(Ver.2)/Script/Distance.cs
+++ Assets/Directory_Prog/melamine/Game(Ver.2)/Script/Distance.cs
@@ -20,25 +20,56 @@ public class Distance : MonoBehaviour
     [SerializeField]
     private string target_SceneName = string.Empty;
 
+    public bool isEnd = false;
+    public bool isSame = false;
+
     void Start()
     {
         image1.SetActive(false);
         image2.SetActive(false);
-        image.color = new Color(0, 0, 0, 0);
+        //image.color = new Color(0, 0, 0, 0);
     }
 
     void Update()
     {
         distance = other.position.x - other1.transform.position.x;
-        distanceText.text = distance.ToString("F1");
+        if(distance>0)
+        {
+            distanceText.text = distance.ToString("F1");
+        }
+        
 
         if(other.position.x<=other1.position.x)
         {
             cam.Follow = null;
+            isSame = true;
             //Board.isEnd = true;
             //image.color = new Color(0, 0, 0, 1);
             //image1.SetActive(true);
             //image2.SetActive(true);
+        }
+
+        if(isSame&&!isEnd)
+        {
+            StartFadeOut();
+            isEnd = true;
+        }
+
+
+    }
+    public void StartFadeOut()
+    {
+        StartCoroutine(FadeCoroutine());
+    }
+
+    IEnumerator FadeCoroutine()
+    {
+        float fadeCount = 0;
+        while (fadeCount < 1.0f)
+        {
+            fadeCount += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            image.color = new Color(0, 0, 0, fadeCount);
         }
     }
 
