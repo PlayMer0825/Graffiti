561b6f78f4a3a6850b4b08325f2c113443fa2f56
kashan2388 <73982847+kashan2388@users.noreply.github.com>
2023-06-14 16:55:23 +0900

    중간 업로드

--- Assets/Directory_Prog/JuKyeung/EventFolder/event_Scripts/CollisionEvent.cs
+++ Assets/Directory_Prog/JuKyeung/EventFolder/event_Scripts/CollisionEvent.cs
@@ -6,6 +6,13 @@ using UnityEngine.Events;
 public class CollisionEvent : MonoBehaviour
 {
     public UnityEvent CollisionStartEvent;
+
+
+    [Header("이동시킬 오브젝트(거의 대부분 플레이어) 가 있을 때")]
+    public Transform targetObject;
+    public Vector3 newPosition;
+    public Quaternion newRotation;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
@@ -15,5 +22,12 @@ public class CollisionEvent : MonoBehaviour
 
         }
     }
+
+    public void MoveObject()
+    {
+        targetObject.position = newPosition;
+        targetObject.rotation = newRotation;
+    }
+
 
 }
