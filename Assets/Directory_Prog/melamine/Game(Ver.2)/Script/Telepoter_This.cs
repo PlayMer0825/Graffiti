798bb006b4cd50c42547344df1f6668ad66afd55
1melamine1 <117522707+1melamine1@users.noreply.github.com>
2023-06-16 07:00:05 +0900

    텔레포트 수정 완

---
+++ Assets/Directory_Prog/melamine/Game(Ver.2)/Script/Telepoter_This.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Telepoter_This : MonoBehaviour
+{
+    public Image image;
+    public static bool telepoter = false;
+
+    IEnumerator FadeCoroutine()
+    {
+        float fadeCount = 1;
+        while (fadeCount > 0f)
+        {
+            fadeCount -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            image.color = new Color(0, 0, 0, fadeCount);
+        }
+    }
+
+
+    private bool isPlayerTriggered = false;
+    [SerializeField]
+    private string target_SceneName = string.Empty;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerTriggered = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isPlayerTriggered)
+        {
+            StartCoroutine(FadeCoroutine());
+            if (target_SceneName == string.Empty)
+                return;
+            SceneManager.LoadScene(target_SceneName);
+            telepoter = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerTriggered = false;
+        }
+    }
+}
