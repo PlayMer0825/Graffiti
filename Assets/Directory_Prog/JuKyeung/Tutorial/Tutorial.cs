ca81f8f7575b571a98206f548ddd5e88a1926818
kashan2388 <73982847+kashan2388@users.noreply.github.com>
2023-06-16 11:28:16 +0900

    튜토리얼 시작 부분 에 추가

--- Assets/Directory_Prog/JuKyeung/Tutorial/Tutorial.cs
+++ Assets/Directory_Prog/JuKyeung/Tutorial/Tutorial.cs
@@ -2,46 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.EventSystems;
 
 public class Tutorial : MonoBehaviour
 {
-    public Image tutorialImage; 
-    public Sprite[] tutorialSprites; 
-    public Button prevButton; 
-    public Button nextButton; 
-
-    private int currentTutorialIndex = 0; 
+    public Image tutorialImage;
+    public Sprite[] tutorialSprites;
+    [SerializeField] private Button prevButton;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button exitButton;
+    private int currentTutorialIndex = 0;
+    private bool isUIActive = true;
 
     private void Start()
     {
-        DisplayTutorialImage(currentTutorialIndex); 
-        UpdateButtonInteractivity(); 
+        DisplayTutorialImage(currentTutorialIndex);
+        UpdateButtonInteractivity();
+
+        if (tutorialSprites.Length <= 1)
+        {
+            prevButton.gameObject.SetActive(false);
+            nextButton.gameObject.SetActive(false);
+            exitButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            prevButton.gameObject.SetActive(currentTutorialIndex > 0);
+            exitButton.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (isUIActive && (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                ShowPreviousTutorialImage();
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+                ShowNextTutorialImage();
+        }
     }
 
     private void UpdateButtonInteractivity()
     {
-        prevButton.interactable = currentTutorialIndex > 0; 
-        nextButton.interactable = currentTutorialIndex < tutorialSprites.Length - 1; 
+        prevButton.interactable = currentTutorialIndex > 0;
+        nextButton.interactable = currentTutorialIndex < tutorialSprites.Length - 1;
     }
 
     private void ShowNextTutorialImage()
     {
-        currentTutorialIndex++; 
+        currentTutorialIndex++;
         DisplayTutorialImage(currentTutorialIndex);
-        UpdateButtonInteractivity(); 
+        UpdateButtonInteractivity();
+
+        if (currentTutorialIndex >= tutorialSprites.Length - 1)
+        {
+            exitButton.gameObject.SetActive(true);
+        }
     }
 
     private void ShowPreviousTutorialImage()
     {
-        currentTutorialIndex--; 
+        currentTutorialIndex--;
         DisplayTutorialImage(currentTutorialIndex);
-        UpdateButtonInteractivity(); 
+        UpdateButtonInteractivity();
+        exitButton.gameObject.SetActive(false);
     }
 
     private void DisplayTutorialImage(int index)
     {
-        tutorialImage.sprite = tutorialSprites[index]; 
+        tutorialImage.sprite = tutorialSprites[index];
     }
 
     public void OnNextButtonClicked()
@@ -52,5 +82,15 @@ public class Tutorial : MonoBehaviour
     public void OnPrevButtonClicked()
     {
         ShowPreviousTutorialImage();
+    }
+
+    public void OnExitButtonClicked()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void SetUIActive(bool active)
+    {
+        isUIActive = active;
     }
 }
