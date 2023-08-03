de7551142cfe93e5fb3ed7e23b8b0ec52e291fec
kashan2388 <defalt730@gmail.com>
2023-08-03 20:59:18 +0900

    매니저 추가

---
+++ Assets/Directory_Prog/JuKyeung/Lucy/Scripts/LucyPuzzleManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LucyPuzzleManager : MonoBehaviour
+{
+    public enum Puzzles
+    {
+       
+    }
+    public UnityEvent LucyPuzzleCompletEvent;
+
+    public static Puzzles LucyPuzzleChecking ( bool _checking)
+    {
+        
+    }
+}
