f055fc5d8b398287b4ea0f0ef1d0fc271f39da3d
kashan2388 <defalt730@gmail.com>
2023-08-07 22:06:10 +0900

    루시 롤러 퍼즐 업로드
    - 현재 루시 씬에 다 배치해서 두지는 못했음
    - 프로그래머 폴더 - 주경  - Lucy - LucyPuzzleTest 씬에서 확인 바람
    - 지워지고 조건 맞춰지면 실행될 수 있는 이벤트까지 구현됨 , 다만 지금 조금 브러쉬 간의 간격이 있는데 이것도 루시 씬에 올리면서 같이 수정해서 올리도록 하겠음
    - 퍼센테이지 계산 안되서 count로 대체함 , 보여지는데는 똑같으나 전체 면적 대비 얼마나 찍어야 되는지 조절해야 함

--- Assets/Scripts/ScratchCardController.cs
+++ Assets/Scripts/ScratchCardController.cs
@@ -7,33 +7,33 @@ using UnityEngine.EventSystems;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class ScratchCardEvent : UnityEvent
-{
-}
-
-public class ScratchCardEvent<T> : UnityEvent<T>
-{
-}
 
 public class ScratchCardController : MonoBehaviour
 {
-    public ScratchCardEvent<float> onScratching { get; set; } = new ScratchCardEvent<float>();
+    public Image Brush; // 스크래치에 사용되는 브러시 이미지를 위한 참조 변수.
 
-    public Image Brush;
+    public RectTransform ScratchArea; // 스크래치 영역을 나타내는 RectTransform 변수.
+    public RenderTexture CoverRenderTexture; // 스크래치 영역을 덮는 RenderTexture 변수.
 
-    public RectTransform ScratchArea;
-    public RenderTexture CoverRenderTexture;
+    private Texture2D tempMaskTexture; // 스크래치 영역의 마스크를 임시로 저장하는 Texture2D 변수.
+    private bool isTouched; // 화면 터치 여부를 확인하는 플래그 변수.
 
-    private Texture2D tempMaskTexture;
-    private bool isTouched;
+     private int Count; // 스크래치한 픽셀의 수를 카운트하는 변수.
 
-    private int Count;
+    private bool[,] matrix; // 스크래치한 픽셀을 추적하기 위한 2D 배열.
 
-    private bool[,] matrix;
+    // public Text progressText; // 스크래치 진행 상태를 표시하는 Text 컴포넌트의 참조 변수.
+    private bool _isprogressTextNull; // progressText가 null인지 확인하는 플래그 변수.
 
-    public Text progressText;
-    private bool _isprogressTextNull;
+    [SerializeField] private GameObject mainCamera;
+    [SerializeField] private GameObject puzzleCamera;
 
+    public UnityEvent scratchEndEvent; // 스크래치 완료 시 트리거되는 UnityEvent.
+
+    public int targetBrushCount; // 몇번의 브러쉬를 찍으면의 조건
+    [SerializeField] private int brushCount;
+
+    // 텍스처를 복제하여 새로운 Texture2D를 생성하는 도우미 메서드.
     Texture2D duplicateTexture(Texture source)
     {
         RenderTexture renderTex = RenderTexture.GetTemporary(
@@ -55,6 +55,8 @@ public class ScratchCardController : MonoBehaviour
         return readableText;
     }
 
+    // RenderTexture를 Texture2D로 변환하는 도우미 메서드.
+
     Texture2D toTexture2D(RenderTexture rTex)
     {
         Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGB24, false);
@@ -66,41 +68,52 @@ public class ScratchCardController : MonoBehaviour
 
     private void Start()
     {
-        _isprogressTextNull = null == progressText;
+        brushCount = 0;
+
+        // progressText가 null인지 확인하여 오류를 방지합니다.
+        // _isprogressTextNull = null == progressText;
+
+        // CoverRenderTexture의 크기를 ScratchArea의 크기와 일치하도록 설정합니다.
         CoverRenderTexture.width = Mathf.RoundToInt(ScratchArea.rect.width) / 2;
         CoverRenderTexture.height = Mathf.RoundToInt(ScratchArea.rect.height) / 2;
+
+        // CanvasScaler의 참조 해상도를 화면 크기와 일치하도록 설정합니다.
         GetComponent<CanvasScaler>().referenceResolution = new Vector2(Screen.width, Screen.height);
+
+        // 스크래치한 픽셀을 추적하기 위한 matrix 배열을 초기화합니다.
         matrix = new bool[CoverRenderTexture.width, CoverRenderTexture.height];
     }
 
     private void Update()
     {
-        if (_isprogressTextNull)
+        if(brushCount >= targetBrushCount)
         {
-            return;
+            scratchEndEvent.Invoke();
         }
-
-        float percent = calculatePercent();
-
-        progressText.text = $"퍼센트 {(percent * 100):#0.00} %";
     }
 
     private void OnEnable()
     {
+        // 객체가 활성화될 때 YieldScratching 코루틴을 시작합니다.
         StartCoroutine(YieldScratching());
+        mainCamera.SetActive(false);
     }
 
     private void OnDisable()
     {
+        // 객체가 비활성화될 때 YieldScratching 코루틴을 정지합니다.
         StopCoroutine(YieldScratching());
+        mainCamera.SetActive(true);
     }
 
+    // 스크래치 과정을 처리하는 코루틴.
     IEnumerator YieldScratching()
     {
         while (true)
         {
             if (isTouched)
             {
+                // 화면이 터치되면 스크래치 과정을 계속합니다.
                 yield return YieldInstantiateScratch();
             }
 
@@ -108,6 +121,7 @@ public class ScratchCardController : MonoBehaviour
             {
                 if (!isTouched)
                 {
+                    // 터치가 시작되면 isTouched 플래그를 true로 설정하고 스크래치를 시작합니다.
                     isTouched = true;
                     Debug.Log("Touch down");
                     yield return YieldInstantiateScratch();
@@ -115,37 +129,51 @@ public class ScratchCardController : MonoBehaviour
             }
             else
             {
+                // 터치가 끝나면 isTouched 플래그를 false로 설정합니다.
                 isTouched = false;
             }
 
+            // 다음 프레임까지 대기합니다.
             yield return null;
         }
     }
 
+    // 브러시를 생성하고 스크래치한 픽셀을 체크하는 코루틴.
     IEnumerator YieldInstantiateScratch()
     {
         Vector2 screenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        // 브러시를 화면 터치 위치에 생성합니다.
         yield return Instantiate(Brush, screenPos, Quaternion.identity, transform);
 
+        // 스크래치한 픽셀을 체크합니다.
         yield return YieldCheckPixelColor();
+
+        // 브러쉬의 카운트 증가합니다. 
+        brushCount += 1;
+
     }
 
+    // CoverRenderTexture의 픽셀 색상을 체크하는 코루틴.
     IEnumerator YieldCheckPixelColor()
     {
+        // 픽셀 색상 체크를 위해 RenderTexture를 Texture2D로 변환합니다.
         yield return tempMaskTexture = toTexture2D(CoverRenderTexture);
 
+        // 각 픽셀을 순회하면서 스크래치 여부를 체크합니다.
         for (int x = 0; x < tempMaskTexture.width; x++)
         {
             for (int y = 0; y < tempMaskTexture.height; y++)
             {
                 if (!matrix[x, y])
                 {
+                    // 아직 스크래치되지 않은 픽셀인 경우, 투명한지(스크래치된 것인지) 체크합니다.
                     if (tempMaskTexture.GetPixel(x, y).r > 0f &&
                         tempMaskTexture.GetPixel(x, y).g > 0f &&
                         tempMaskTexture.GetPixel(x, y).b > 0f &&
                         tempMaskTexture.GetPixel(x, y).a > 0f)
                     {
+                        // 투명하다면 Count를 증가시키고 해당 픽셀을 스크래치된 것으로 표시합니다.
                         Count++;
                         matrix[x, y] = true;
                     }
@@ -153,21 +181,25 @@ public class ScratchCardController : MonoBehaviour
             }
         }
 
+        // 다음 프레임까지 대기합니다.
         yield return null;
     }
 
     private void OnGUI()
     {
-//        GUIStyle style = new GUIStyle()
-//            {fontSize = 20, fontStyle = FontStyle.Bold, normal = new GUIStyleState() {textColor = Color.black}};
-//
-//        float percent = calculatePercent();
-//
-//        GUILayout.Label($"總共刮了 {(percent * 100).ToString("#0.00")} %", style);
+        //GUIStyle style = new GUIStyle()
+        //{ fontSize = 20, fontStyle = FontStyle.Bold, normal = new GUIStyleState() { textColor = Color.black } };
+
+        //float percent = calculatePercent();
+        //
+        //        GUILayout.Label($"總共刮了 {(percent * 100).ToString("#0.00")} %", style);
     }
 
-    private float calculatePercent()
-    {
-        return (float) Count / (CoverRenderTexture.width * CoverRenderTexture.height);
-    }
+    // 스크래치 영역의 백분율을 계산합니다.
+    //private float calculatePercent()
+    //{
+    //    return (float)Count / (CoverRenderTexture.width * CoverRenderTexture.height);
+    //}
+
+
 }
